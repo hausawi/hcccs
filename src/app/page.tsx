06107b727f2350fddafc1beacb850ce9f83cb450
Hello there...! 👋🏽 welcome to my profile <haidermoubarak1987@gmail.com>
2025-05-06 17:05:00 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Homepage = () => {
   return (
-    <div className='flex item-center justify-center w'>  <h3 className="text-blue-300">
+    <div>  <h3 className="text-blue-700">
       <br/><br/><br/>go to to see the change
       https://hausawi.github.io/hcccs/dashboard/admin</h3> 
       </div>
